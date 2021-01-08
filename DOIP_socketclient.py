503b967e9a8a87c8d4205d0a56f14a991391ef6d
uschwar1 <uschwar1@gwdg.de>
2021-01-08 17:30:40 +0100

    DOIP_socketclient.py uses now DOIPio.py and asks for Op.Hello and Op.ListOperations

--- DOIP_socketclient.py
+++ DOIP_socketclient.py
@@ -1,28 +1,64 @@
 import socket
 import ssl
+import json
+
+import DOIPio
 
 fn = "DOIPrequests/Hello_Request.doip"
 inData = ''
 with open(fn) as fin:
     for line in fin:
         inData += line
+print("retrieving request from:", fn)
 
-# for wrong targetId use:
-# inData = '{\n  "targetId": "20.500.124/service",\n  "operationId": "0.DOIP/Op.Hello"\n}\n#\n#\n'
+class DOIPServerConfig():
 
-server_port = 8443
-server_addr = 'localhost'
+    def __init__(self):
+        self.service_ids = ["0.DOIP/Op.Hello@20.500.123/service", "0.DOIP/Op.Create@20.500.123/service", "0.DOIP/Op.Retrieve@20.500.123/service", "0.DOIP/Op.Update@20.500.123/service", "0.DOIP/Op.Delete@20.500.123/service", "0.DOIP/Op.Search@20.500.123/service", "0.DOIP/Op.ListOperations@20.500.123/service"]
+        self.listen_addr = 'localhost'
+        self.listen_port = 8443
+        self.server_cert = 'certs/server.crt'
+        self.client_key = 'certs/client1.key'
+        self.client_cert = 'certs/client1.crt'
+        self.context_verify_mode = ssl.CERT_REQUIRED
+
+
+config = DOIPServerConfig()
+                 
 ctx = ssl.create_default_context()
-ctx.verify_mode = ssl.CERT_REQUIRED
+ctx.verify_mode = config.context_verify_mode
 ctx.check_hostname = True
-ctx.load_verify_locations("certs/server.crt")
-ctx.load_cert_chain(certfile="certs/client1.crt", keyfile="certs/client1.key")
+ctx.load_verify_locations(config.server_cert)
+ctx.load_cert_chain(certfile=config.client_cert, keyfile=config.client_key)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    with ctx.wrap_socket(sock, server_hostname=server_addr) as ssl_sock:
-        ssl_sock.connect((server_addr, server_port))
+    with ctx.wrap_socket(sock, server_hostname=config.listen_addr) as ssl_sock:
+        ssl_sock.connect((config.listen_addr, config.listen_port))
+        
         ssl_sock.sendall(bytes(inData, "utf-8"))
+        rfile = ssl_sock.makefile('rb', 1024)
+        DOIPRequest = DOIPio.InputMessageProcessing(ssl_sock,rfile,config)
+        # now listen to server
+        segment_1st = DOIPRequest.getLeadingSegment()
+        janswer = DOIPRequest._segment_to_json(segment_1st)
+        print (json.dumps(janswer, sort_keys=True,indent=2, separators=(',', ' : ')))
 
-        data = ssl_sock.read()
-        print (data)
+        
+fn = "DOIPrequests/ListOperations_Request.doip"
+print("retrieving request from:", fn)
+inData = ''
+with open(fn) as fin:
+    for line in fin:
+        inData += line
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    with ctx.wrap_socket(sock, server_hostname=config.listen_addr) as ssl_sock:
+        ssl_sock.connect((config.listen_addr, config.listen_port))
+        ssl_sock.sendall(bytes(inData, "utf-8"))
+        rfile = ssl_sock.makefile('rb', 1024)
+        DOIPRequest = DOIPio.InputMessageProcessing(ssl_sock,rfile,config)
+        # now listen to server
+        segment_1st = DOIPRequest.getLeadingSegment()
+        janswer = DOIPRequest._segment_to_json(segment_1st)
+        print (json.dumps(janswer, sort_keys=True,indent=2, separators=(',', ' : ')))
         
