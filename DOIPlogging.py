e448508a216857d583b44d9b122421c810fd2dc2
uschwar1 <uschwar1@gwdg.de>
2023-05-11 17:43:41 +0200

    add DOIPlogging.py __init__.py

---
+++ DOIPlogging.py
@@ -0,0 +1,23 @@
+'''
+Created on 18.09.2022
+
+@author: Ulrich Schwardmann, Göttingen
+'''
+import sys
+import logging
+
+class config(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        logging.basicConfig(
+            # level=logging.INFO,
+            level=logging.DEBUG,
+            format='%(asctime)s - %(levelname)s - %(name)s: %(message)s',
+            stream=sys.stderr,
+        )
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This is a python implementation of DOIP server.
 It implements an SSL server with authentication and authorization based on python3 socketserver and
 ssl.
 
-It uses a ThreadingMixIn - TCPServer as RequestServer.
+It uses an asynchronious RequestServer based on asyncio.
